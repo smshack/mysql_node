b3217b3626752262ef4e25085a9cac38b853421d
smshack <5432tat@naver.com>
2021-02-14 06:37:58 +0000

    mysql 시작 연결

---
+++ README.md
@@ -0,0 +1,7 @@
+# 1. mysql 모듈 다운
+yarn add mysql
+
+
+https://www.npmjs.com/package/mysql
+
+node mysql.js
--- main.js
+++ main.js
@@ -140,4 +140,4 @@ var app = http.createServer(function(request,response){
       response.end('Not found');
     }
 });
-app.listen(3000);
+app.listen(4000);
